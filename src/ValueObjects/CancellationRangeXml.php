f5fd0585af9a1a804ca0b25d88e62ac17f59bfb6
warlyn <warlyn200@gmail.com>
2026-04-14 07:35:11 -0400

    feat: add XML parsing methods to CancellationRangeXml and signature removal to InvoiceXml

--- src/ValueObjects/CancellationRangeXml.php
+++ src/ValueObjects/CancellationRangeXml.php
@@ -15,4 +15,50 @@ class CancellationRangeXml
     {
         $this->xml = simplexml_load_string($xml);
     }
+
+    public function getTotal(): ?int
+    {
+        if (!empty($this->xml?->Encabezado?->CantidadeNCFAnulados)) {
+            return (int)$this->xml?->Encabezado?->CantidadeNCFAnulados;
+        }
+
+        return null;
+    }
+
+    public function getDate(): ?string
+    {
+        if (!empty($this->xml?->Encabezado?->FechaHoraAnulacioneNCF)) {
+            return (string)$this->xml?->Encabezado?->FechaHoraAnulacioneNCF;
+        }
+
+        return null;
+    }
+
+    public function getDetails(): array
+    {
+        $details = [];
+
+        if (!empty($this->xml?->DetalleAnulacion?->Anulacion)) {
+            foreach ($this->xml?->DetalleAnulacion?->Anulacion as $anulacion) {
+                $sequences = [];
+                if (!empty($anulacion->TablaRangoSecuenciasAnuladaseNCF?->Secuencias)) {
+                    foreach ($anulacion->TablaRangoSecuenciasAnuladaseNCF?->Secuencias as $seq) {
+                        $sequences[] = [
+                            'SecuenciaeNCFDesde' => (string)$seq->SecuenciaeNCFDesde,
+                            'SecuenciaeNCFHasta' => (string)$seq->SecuenciaeNCFHasta,
+                        ];
+                    }
+                }
+
+                $details[] = [
+                    'NoLinea' => (int)$anulacion->NoLinea,
+                    'TipoeCF' => (string)$anulacion->TipoeCF,
+                    'CantidadeNCFAnulados' => (int)$anulacion->CantidadeNCFAnulados,
+                    'Secuencias' => $sequences,
+                ];
+            }
+        }
+
+        return $details;
+    }
 }
